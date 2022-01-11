1e958d8cb1238553b6a110126fdc6d1981dd9870
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-01-11 17:12:45 -0300

    settingChangesOnAdapter

--- app/src/main/java/com/LiveAdapter.kt
+++ app/src/main/java/com/LiveAdapter.kt
@@ -1,21 +1,52 @@
 package com
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewstructure.R
 import com.model.Live
+import kotlinx.android.synthetic.main.res_item_live.view.*
 
 class LiveAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var items : List<Live> = ArrayList()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+
+        return LiveViewHolder( //definimos o layout do nosso viewHolder
+            LayoutInflater.from(parent.context).inflate(R.layout.res_item_live, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        when(holder){
+
+            is LiveViewHolder -> {
+                holder.bind(items[position])
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
+    }
+
+    class LiveViewHolder constructor(
+        itemView : View //preciso passar essa implementaçao do ViewHolder nessa classe tbm.
+    ) : RecyclerView.ViewHolder(itemView){
+
+        private val liveTitle = itemView.title
+        private val liveAuthor = itemView.author
+        private val liveThumbnail = itemView.thumbnail
+
+        //a funcao bind pega as informacoes da model que vou passar pra ele e coloca no layout do item do RecyclerView
+        fun bind(live : Live) {
+
+            liveTitle.text = live.title
+            liveAuthor.text = live.author
+
+        }
+
     }
 }
